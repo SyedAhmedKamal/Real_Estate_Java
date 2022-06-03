5597d7f8a04edee7944d8238a52c56470d4e4889
IamSyedAhmed <kamalahmed853@gmail.com>
2022-06-03 20:49:17 +0500

    registration screen added

--- app/src/main/java/com/example/realestate_java/view/SignInActivity.java
+++ app/src/main/java/com/example/realestate_java/view/SignInActivity.java
@@ -2,17 +2,26 @@ package com.example.realestate_java.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.realestate_java.R;
+import com.example.realestate_java.databinding.ActivitySignInBinding;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private ActivitySignInBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         getSupportActionBar().hide();
+
+        binding.signUpBtn.setOnClickListener(view -> {
+            startActivity(new Intent(SignInActivity.this, SignUpActivity.class));
+        });
     }
 }
---
+++ app/src/main/java/com/example/realestate_java/view/SignUpActivity.java
@@ -0,0 +1,26 @@
+package com.example.realestate_java.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.realestate_java.R;
+import com.example.realestate_java.databinding.ActivitySignUpBinding;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportActionBar().hide();
+
+        binding.backToLogin.setOnClickListener(view -> {
+            finish();
+        });
+    }
+}
